3a5e78426e39d5403f2f6cbee16d9bb08e004d68
yungxinyang <69131715+yungxinyang@users.noreply.github.com>
2020-08-05 14:24:13 +0800

    Add files via upload

---
+++ day3-4.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 10:47:23 2020
+
+@author: user
+"""
+
+b={
+        'me':'0917',
+        'mom':'1025',
+        'dad':'0104',
+        'elder sis':'0627',
+        'y sis':'1030',
+        'grandma':'1227',
+        'grandpa':'0222',
+        }
+b[1]='0901'
+b.pop('me')
+print(b)
