44fbf8385cc6ab43e7f63c93e05dd4163e6acf8f
yungxinyang <69131715+yungxinyang@users.noreply.github.com>
2020-08-05 10:24:04 +0800

    Add files via upload

---
+++ day3-2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 09:45:24 2020
+
+@author: user
+"""
+
+x=int(input('num1'))
+y=int(input('num2'))
+z=int(input('num3'))
+def add3(a,b,c):
+    return a+b+c
+print(add3(x,y,z))
